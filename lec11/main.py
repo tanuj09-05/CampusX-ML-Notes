f14c75c6704a90bb9390156a4449d2aec9030774
Tanuj Bisht <btanuj25@gmail.com>
2026-06-09 19:02:45 +0530

    Add lecture 11 notes

---
+++ lec11/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# creating 0d tensor 
+a = np.array(4)
+print(a.ndim)
+
+# creating 1D tensor
+b= np.array([1,34,53,12,7])
+print(b.ndim)
+
+# creating 2D tensor
+c= np.array([[1,34,53,12,7], [2,4,5,43,1]])
+print(c.ndim)
